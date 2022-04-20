b4d8c8842508cba268245d40499d878834017819
febarros <febarros@uolinc.com>
2022-04-20 16:30:02 -0300

    update: JSDOCS

--- core/NewsletterFile.js
+++ core/NewsletterFile.js
@@ -8,16 +8,19 @@ const ENCODE = {
 }
 
 class NewsletterFile {
-
-    constructor(columnists = []) {
-        this._columnists = columnists
+    /**
+     * @param  Array dataList=[]
+     */
+    constructor(dataList = []) {
     }
 
     /**
-     *  Método seta nome
+     *  Método seta compoem nome apartir de slug e uma extensao
      *  
-     * @param  {} name
-     * @param  {} extension='xml'
+     * @param  String name : obrigatorio
+     * @param  String extension : caso omitido padrao é xml
+     * 
+     * @returns String 
      */
     static composeFileName(name, extension = 'xml') {
         return `${name}.${extension}`
@@ -26,8 +29,10 @@ class NewsletterFile {
     /**
      *  Método criar um arquivo no caminho setado no paramentro
      * 
-     * @param  String output_path
-     * @param  String content
+     * @param  String output_path : obrigatorio
+     * @param  String content : obrigatorio
+     * @param  String name : caso omitido padrao é newsletter
+     * @param  String encode : caso omitido padrao é iso-8859-1
      * 
      * @returns blob
      * 
@@ -43,9 +48,17 @@ class NewsletterFile {
     }
 
     /**
-     * Método responsavel por criar pasta no path como o nome igual ao slug passado por paramentro
-     * @param  String path
-     * @param  String slug
+     * Método responsável por criar pasta no path como o nome 
+     * igual ao slug passado por paramentro
+     * 
+     * @example
+     *  
+     *  Newsletter.createFile('./documentos/', 'nome-pasta')  retorno : './documentos/nome-pasta'
+     * 
+     * @param  String path : obrigatorio
+     * @param  String slug : obrigatorio
+     * 
+     * @returns String 
      */
     static createFolder(path, slug) {
         const dir = `${path}uol-${slug}/`
@@ -62,6 +75,7 @@ class NewsletterFile {
      *  @example 
      *      ./coluna-juca/newsletter.xml
      * 
+     * @param  Function template : obrigatorio
      * @param  String output_path
      * @param  Function template
      */
