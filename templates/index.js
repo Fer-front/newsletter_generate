f61a24963c36df246385fea0a773be83d652f4d9
febarros <febarros@uolinc.com>
2022-04-21 12:35:55 -0300

    add arquivo de resgistro de tpls

---
+++ templates/index.js
@@ -0,0 +1,9 @@
+// TODO: todos templates devem ser importados e exportado neste arquivo
+
+const colunist = require('./colunistTemplate')
+const teste = require('./ModeloTemplate')
+
+module.exports = {
+    colunist,
+    teste
+}
