c85fc5d8ed589dc6b6032b2943b783fd103c010a
febarros <febarros@uolinc.com>
2022-04-21 12:49:32 -0300

    refactory: movendo template para arquivo isolado

--- core/commandCreateTemplate.js
+++ core/commandCreateTemplate.js
@@ -1,36 +1,9 @@
-const { ENCODE } = require('./Enums')
+const tpl = require('../helps/tpl')
+const { ENCODE } = require('../helps/Enums')
+
 const readline = require('readline');
 
 const NewsletterFile = require('./NewsletterFile')
-
-function template(className) {
-    return `
-// [ICreateTemplate] garante que os metodos template createFile seja implementados
-// [NewsletterFile] possui metodos para criaçao de arquivos com encode compativel em newsletter
-const ICreateTemplate = require('../core/ITemplate')
-const NewsletterFile = require('../core/NewsletterFile')
-
-
-// TODO: OBS para utilizacao e melhor organizacao após criar seu template
-// registre o mesmo no arquivo ./index.js
-
-class ${className} extends ICreateTemplate {
-    constructor() {
-        super()
-    }
-
-    template() {
-        // TODO: IMPLEMENTAR TEMPLATE
-    }
-
-    createFile(data) {
-       // TODO: IMPLEMENTAR CREATE-FILE
-    }
-}
-
-module.exports = ${className}
-    `
-}
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -38,7 +11,7 @@ const rl = readline.createInterface({
 });
 
 rl.question('Digite o nome do arquivo de Template Ex: colunaTemplate: \n', (name) => {
-    NewsletterFile.createFile('./templates/', template(name), name, 'js', ENCODE.UTF8)
+    NewsletterFile.createFile('./templates/', tpl(name), name, 'js', ENCODE.UTF8)
     rl.close();
 })
 
