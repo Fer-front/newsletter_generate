d257c124573142757ff22f002017607f2f03e316
febarros <febarros@uolinc.com>
2022-04-25 15:23:25 -0300

    fix: ajuste index.js

--- templates/index.js
+++ templates/index.js
@@ -1,7 +1,12 @@
 // TODO: todos templates devem ser importados e exportado neste arquivo
 
+// OBS: nao remova os comentario ele são utilizado para automatizar registro
+//      dos template automaticamente.
+
 const colunist = require('./colunistTemplate')
+//newTemplateImport
 
 module.exports = {
     colunist,
+    //newTemplateRegister
 }
